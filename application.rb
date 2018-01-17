112a9f9b218836647dc38b2df1640f8e8b040f4e
Shaban Karumba <shaban.karumba@fundingcircle.com>
2018-01-17 21:18:26 +0000

    Rename endpoints to specify thier roles

--- application.rb
+++ application.rb
@@ -4,10 +4,10 @@ require_relative 'lib/url_shortner'
 class Application < Sinatra::Base
   url_shortner = URLShortner.new
 
-  post '/' do
+  post '/api/url' do
     request_body = JSON.parse(request.body.read)
     url = request_body['url']
-    halt 401, { error: 'Please provide a url' }.to_json if url.nil?
+    halt 400, { error: 'Please provide a url' }.to_json if url.nil?
 
     generated_short_url = url_shortner.generate(url)
     short_url = "/#{generated_short_url}"
@@ -25,7 +25,7 @@ class Application < Sinatra::Base
     redirect to(matched_url), 301
   end
 
-  get '/shortened_urls' do
+  get '/api/urls' do
     url_shortner.urls.to_json
   end
 end
--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Application do
   let(:app) { Application.new }
   let(:url) { 'http://www.farmdrop.com' }
   
-  describe 'POST /' do
+  describe 'POST /api/url' do
     context 'when valid attributess are provided' do
       it 'responds successfully' do
-        post('/', JSON.generate({ url: url }))
+        post('/api/url', JSON.generate({ url: url }))
         expect(last_response.status).to eq(201)
         expect(JSON.parse(last_response.body)).to include(
           { 'short_url' => be_a(String), 'url' => url }
@@ -18,8 +18,8 @@ RSpec.describe Application do
     
     context 'when invalid attributes are provided' do
       it 'responds unsuccessfully' do
-        post('/', '{}')
-        expect(last_response.status).to eq(401)
+        post('/api/url', '{}')
+        expect(last_response.status).to eq(400)
         expect(JSON.parse(last_response.body)).to eq(
           { 'error' => 'Please provide a url' }
         )
@@ -30,12 +30,12 @@ RSpec.describe Application do
   describe 'GET /' do
     context 'when the short url exists' do
       let(:shortend_url) do
-        get('/shortened_urls')
+        get('/api/urls')
         JSON.parse(last_response.body).keys.first
       end
 
       before do
-        post '/', JSON.generate({ url: url })
+        post '/api/url', JSON.generate({ url: url })
       end
 
       it 'redirects to the url' do
@@ -53,9 +53,9 @@ RSpec.describe Application do
     end
   end
 
-  describe 'GET /shortened_urls' do
+  describe 'GET /api/urls' do
     it 'responds successfully' do
-      get('shortened_urls')
+      get('/api/urls')
       expect(JSON.parse(last_response.body)).to be_an(Hash)
     end
   end
